f91c55916803f9d40d9a6f9ea05b5f7955f2b1d0
SolalDR <solal.dussout-revel@hotmail.fr>
2017-03-22 19:34:57 +0100

    Angular Works

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  root "application#home"
+
   resources :snippet_versions
   resources :snippets
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
