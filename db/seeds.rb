dd8645f1e6739ceaf1f683b92193789c1e28f6d5
Sébastien GAYA <somniia.99@gmail.com>
2017-04-05 17:01:29 +0000

    bgColor & textColor for lang seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,16 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # Intégration
-Lang.create(cd: "html", lib: "html", bgColor: "#FFFFFF", color: "#000000", cdMirror:"htmlmixed");
-Lang.create(cd: "js", lib: "javascript", bgColor: "#FFFFFF", color: "#000000", cdMirror:"javascript");
-Lang.create(cd: "css", lib: "css", bgColor: "#FFFFFF", color: "#000000", cdMirror:"x-scss");
-Lang.create(cd: "scss", lib: "scss", bgColor: "#FFFFFF", color: "#000000", cdMirror:"scss");
-Lang.create(cd: "sass", lib: "sass", bgColor: "#FFFFFF", color: "#000000", cdMirror:"javascript");
-Lang.create(cd: "json", lib: "json", bgColor: "#FFFFFF", color: "#000000", cdMirror:"json");
+Lang.create(cd: "html", lib: "html", bgColor: "#E44B23", color: "#ffffff", cdMirror:"htmlmixed");
+Lang.create(cd: "js", lib: "javascript", bgColor: "#f1e05a", color: "#000000", cdMirror:"javascript");
+Lang.create(cd: "css", lib: "css", bgColor: "#563d7c", color: "#ffffff", cdMirror:"x-scss");
+Lang.create(cd: "scss", lib: "scss", bgColor: "#CF649A", color: "#ffffff", cdMirror:"scss");
+Lang.create(cd: "sass", lib: "sass", bgColor: "#CF649A", color: "#ffffff", cdMirror:"javascript");
+Lang.create(cd: "json", lib: "json", bgColor: "#f1e05a", color: "#000000", cdMirror:"json");
 
 # Back
-Lang.create(cd: "php", lib: "php", bgColor: "#FFFFFF", color: "#000000", cdMirror:"php");
-Lang.create(cd: "rb", lib: "ruby", bgColor: "#FFFFFF", color: "#000000", cdMirror:"javascript");
-Lang.create(cd: "java", lib: "java", bgColor: "#FFFFFF", color: "#000000", cdMirror:"javascript");
-Lang.create(cd: "c", lib: "c", bgColor: "#FFFFFF", color: "#000000", cdMirror:"javascript");
-Lang.create(cd: "swift", lib: "swift", bgColor: "#FFFFFF", color: "#000000", cdMirror:"javascript");
+Lang.create(cd: "php", lib: "php", bgColor: "#4F5D95", color: "#ffffff", cdMirror:"php");
+Lang.create(cd: "rb", lib: "ruby", bgColor: "#701516", color: "#ffffff", cdMirror:"javascript");
+Lang.create(cd: "java", lib: "java", bgColor: "#b07219", color: "#ffffff", cdMirror:"javascript");
+Lang.create(cd: "c", lib: "c", bgColor: "#555555", color: "#ffffff", cdMirror:"javascript");
+Lang.create(cd: "swift", lib: "swift", bgColor: "#ffac45", color: "#000000", cdMirror:"javascript");
