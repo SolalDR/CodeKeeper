86cc284cb062fb84990d14c8f44af5c84d94db7d
SolalDR <solal.dussout-revel@hotmail.fr>
2017-04-25 19:34:56 +0200

    Add login with github after persisted

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,7 +1,15 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def github
-    @user = User.from_omniauth(request.env["omniauth.auth"])
+    auth = request.env["omniauth.auth"]
+    if User.find_by(email: auth.info.email)
+      @user = User.find_by(email: auth.info.email)
+      @user.provider = auth.provider
+      @user.uid = auth.uid
+      @user.save
+    else
+      @user = User.from_omniauth(auth)
+    end
     sign_in_and_redirect @user
   end
 
